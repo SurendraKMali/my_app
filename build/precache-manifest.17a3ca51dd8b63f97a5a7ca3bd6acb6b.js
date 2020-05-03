c684f3ae6fcf07cf31d1ae07c9eb78176dda62ea
SurendraKMali <37996959+SurendraKMali@users.noreply.github.com>
2020-05-04 00:29:57 +0530

    initial commit

---
+++ build/precache-manifest.17a3ca51dd8b63f97a5a7ca3bd6acb6b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b352d9d5377c7f7ee2b22aa6ecba2814",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b0615450b2bdeb13f42",
+    "url": "/static/css/main.568758be.chunk.css"
+  },
+  {
+    "revision": "17c79084c1e20441ba34",
+    "url": "/static/js/2.a489c2db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a489c2db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0615450b2bdeb13f42",
+    "url": "/static/js/main.5f246e29.chunk.js"
+  },
+  {
+    "revision": "2060b9a7e54a59ab7b07",
+    "url": "/static/js/runtime-main.f3a02c50.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
